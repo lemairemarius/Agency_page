ad598e16197996f00c36dbecc74f75060b4854fe
lemairemarius <79089751+lemairemarius@users.noreply.github.com>
2021-08-17 09:58:31 +0200

    corect chemin

---
+++ precache-manifest.e567696c3ee897fce3586361281979b3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "143fe65fc9dfccf506784f4c252fc465",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0452772d2cfb2bd2e784",
+    "url": "./static/css/main.ee0979bb.chunk.css"
+  },
+  {
+    "revision": "7cc25a584f1bc12d0515",
+    "url": "./static/js/2.bb7e1676.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.bb7e1676.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0452772d2cfb2bd2e784",
+    "url": "./static/js/main.a58d3968.chunk.js"
+  },
+  {
+    "revision": "b2818d753e5d371de03d",
+    "url": "./static/js/runtime-main.d656796d.js"
+  },
+  {
+    "revision": "6f82017092b3f62c335565b373a29482",
+    "url": "./static/media/KronaOne-Regular.6f820170.ttf"
+  },
+  {
+    "revision": "37c8a1aaa2c1891aa0960ff03c377649",
+    "url": "./static/media/Questrial-Regular.37c8a1aa.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2df0f597ae2804d74c5f1564e27ed4aa.js"
+  "./precache-manifest.e567696c3ee897fce3586361281979b3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
